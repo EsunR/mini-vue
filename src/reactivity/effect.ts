65a9ba6d971217a0e6a20c0967dc945d04673839
EsunR <esunr641@icloud.com>
2024-02-28 22:52:26 +0800

    project init

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,55 @@
+class ReactiveEffect {
+  private _fn: Function;
+  constructor(fn: Function) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+/**
+ * 存放结构：
+ * TargetMap: Map<原始对象 target, DepsMap>
+ * DepsMap: Map<属性值 key, Dep>
+ * Dep: Set<依赖函数>
+ */
+type TargetMap = Map<any, DepsMap>;
+type DepsMap = Map<any, DepSet>;
+type DepSet = Set<ReactiveEffect>;
+const targetMap: TargetMap = new Map();
+
+export function track(target, key) {
+  // 获取到当前 target（原始对象）在 targetMap 中的依赖集合
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  // 获取到当前 key（属性值）在 depsMap 中的依赖集合
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+
+  const dep = depsMap.get(key);
+  if (!dep) return;
+
+  dep.forEach((effect: ReactiveEffect) => {
+    effect.run();
+  });
+}
+
+let activeEffect: ReactiveEffect;
+
+export function effect(fn: Function) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,24 @@
+import { track, trigger } from "./effect";
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      /**
+       * ES6 Proxy里面为什么要用Reflect: https://www.zhihu.com/question/460133198
+       */
+      const res = Reflect.get(target, key);
+
+      // 依赖收集
+      track(target, key);
+      return res;
+    },
+
+    set(target, key, value) {
+      const res = Reflect.set(target, key, value);
+
+      // 触发依赖
+      trigger(target, key);
+      return res;
+    },
+  });
+}
