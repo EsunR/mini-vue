d060376affc55e6f5574c77a1fc1cab03c26dfe4
EsunR <esunr641@icloud.com>
2024-02-29 20:24:17 +0800

    实现 effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -52,4 +52,5 @@ let activeEffect: ReactiveEffect;
 export function effect(fn: Function) {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  return _effect.run.bind(_effect);
 }
