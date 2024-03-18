0fc1bc55a06b37eeaf0cafa065d7df763add916b
EsunR <esunr641@icloud.com>
2024-03-18 23:03:57 +0800

    14. 实现 shallowReadonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,10 +1,19 @@
-import { isObject } from "../shared";
+import { extend, isObject } from "../shared";
 import { track, trigger } from "./effect";
 import { ReactiveFlags, reactive, readonly } from "./reactive";
 
-function createGetter(isReadonly = false): ProxyHandler<any>["get"] {
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+const shallowReadonlyGet = createGetter(true, true);
+
+function createGetter(
+  isReadonly = false,
+  shallow = false
+): ProxyHandler<any>["get"] {
   return function get(target, key) {
     if (key === ReactiveFlags.IS_REACTIVE) {
+      // 是 readonly 的话就不是 reactive 了
       return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly;
@@ -14,6 +23,11 @@ function createGetter(isReadonly = false): ProxyHandler<any>["get"] {
      * ES6 Proxy里面为什么要用Reflect: https://www.zhihu.com/question/460133198
      */
     const res = Reflect.get(target, key);
+
+    // TODO: 这里只做了 shallowReadonly，因此直接返回 res 而不进行 track
+    if (shallow) {
+      return res;
+    }
 
     // 相比于 vue2，vue3 采用懒加载，当访问响应式对象时，再去构造下一个Proxy(res, { getter })
     if (isObject(res)) {
@@ -39,16 +53,23 @@ function createSetter(isReadonly = false): ProxyHandler<any>["set"] {
   };
 }
 
-const get = createGetter();
-const set = createSetter();
-const readonlyGet = createGetter(true);
-
 export const mutableHandlers: ProxyHandler<any> = { get, set };
 
 export const readonlyHandlers: ProxyHandler<any> = {
   get: readonlyGet,
-  set() {
-    console.warn("readonly");
+  set(target, key) {
+    console.warn(
+      `Set operation on key "${String(key)}" failed: target is readonly.`,
+      target
+    );
     return true;
   },
 };
+
+export const shallReadonlyHandlers: ProxyHandler<any> = extend(
+  {},
+  readonlyHandlers,
+  {
+    get: shallowReadonlyGet,
+  }
+);
---
+++ src/reactivity/tests/shallowReadonly.spec.ts
@@ -0,0 +1,18 @@
+import { isReactive, isReadonly, shallowReadonly } from "../reactive";
+
+describe("shallowReadonly", () => {
+  test("should not make non-reactive properties reactive", () => {
+    const props = shallowReadonly({ n: { foo: 1 } });
+    expect(isReadonly(props)).toBe(true);
+    expect(isReadonly(props.n)).toBe(false);
+  });
+
+  it("warn then call set", () => {
+    console.warn = jest.fn();
+    const original: any = { foo: 1 };
+    const observed = shallowReadonly(original);
+    observed.foo = 2;
+    expect(console.warn).toBeCalled();
+    expect(original.foo).toBe(1);
+  });
+});
