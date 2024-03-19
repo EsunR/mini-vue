2154e9ca23d4e40b21ea2e467ac763d135ae931e
EsunR <esunr641@icloud.com>
2024-03-20 00:32:01 +0800

    17. 实现 isRef 和 unref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,15 +2,19 @@ import { hasChanged, isObject } from "../shared";
 import { DepSet, isTracking, trackEffects, triggerEffects } from "./effect";
 import { reactive } from "./reactive";
 
-class RefImpl {
+export type MaybeRef<T = any> = T | Ref<T>;
+
+class Ref<T = any> {
   /**  存储值 */
-  private _value: any;
+  private _value: T;
   /**  使用 Ref 包裹的原始值/对象 */
-  private _rawValue: any;
+  private _rawValue: T;
   /** 该值的依赖队列 */
   public depSet: DepSet = new Set();
 
-  constructor(value: any) {
+  public __v_isRef = true;
+
+  constructor(value: T) {
     // 如果 value 是一个对象，则使用 reactive 进行包裹
     this._value = isObject(value) ? reactive(value) : value;
     // 对原始对象进行存储
@@ -32,12 +36,20 @@ class RefImpl {
   }
 }
 
-function trackRefValue(ref: RefImpl) {
+function trackRefValue(ref: Ref) {
   if (isTracking()) {
     trackEffects(ref.depSet);
   }
 }
 
-export function ref(value: any) {
-  return new RefImpl(value);
+export function ref<T = any>(value: T) {
+  return new Ref<T>(value);
+}
+
+export function isRef(ref: any) {
+  return !!ref.__v_isRef;
+}
+
+export function unref<T>(ref: MaybeRef<T>): T {
+  return isRef(ref) ? (ref as Ref<T>).value : (ref as T);
 }
