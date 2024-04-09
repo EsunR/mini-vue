b199fec90637e752865d7cea27d9c3c6fcf4568f
EsunR <esunr641@icloud.com>
2024-04-10 00:01:37 +0800

    22. 实现初始化 element

--- example/helloword/App.js
+++ example/helloword/App.js
@@ -2,7 +2,29 @@ import { h } from "../../lib/mini-vue.esm.js";
 
 export const App = {
     render() {
-        return h("div", "hi, " + this.msg);
+        return h(
+            "div",
+            {
+                id: "root",
+                class: ["blue", "hard"],
+            },
+            [
+                h(
+                    "p",
+                    {
+                        class: ["red"],
+                    },
+                    "hello!",
+                ),
+                h(
+                    "p",
+                    {
+                        class: ["blue"],
+                    },
+                    "this is your message: xxx",
+                ),
+            ],
+        );
     },
     setup() {
         return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -5,10 +5,52 @@ export function render(vnode, container: HTMLElement) {
     patch(vnode, container);
 }
 
+/**
+ * 将 vnode 渲染到 container 中
+ */
 function patch(vnode, container: HTMLElement) {
-    // TODO: 区分是组件还是普通元素
-    // processElement(vnode, container);
-    processComponent(vnode, container);
+    if (typeof vnode.type === "string") {
+        processElement(vnode, container);
+    } else {
+        processComponent(vnode, container);
+    }
+}
+
+function processElement(vnode, container: HTMLElement) {
+    mountElement(vnode, container);
+}
+
+function mountElement(vnode, container: HTMLElement) {
+    // 创建元素
+    const el = document.createElement(vnode.type) as HTMLElement;
+
+    // 挂载子节点
+    mountChildren(vnode, el);
+
+    // 处理属性
+    const { props } = vnode;
+    for (const key in props) {
+        el.setAttribute(key, props[key]);
+    }
+
+    // 挂载
+    container.append(el);
+}
+
+/**
+ * 将 vnode 的子节点挂载到 container 中
+ */
+function mountChildren(vnode, container: HTMLElement) {
+    const { children } = vnode;
+    if (typeof children === "string") {
+        container.textContent = children;
+    } else if (children instanceof Array) {
+        children.forEach((vnodeChild) => {
+            patch(vnodeChild, container);
+        });
+    } else if (typeof children === "object") {
+        patch(children, container);
+    }
 }
 
 function processComponent(vnode, container: HTMLElement) {
